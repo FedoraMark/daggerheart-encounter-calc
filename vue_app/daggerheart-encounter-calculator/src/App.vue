f26782cca4fc287496f186cd426cd658cc5d9d7a
FedoraMark <fedora.mark@icloud.com>
2025-06-25 13:22:03 -0400

    [dh-vue] start VUE app

---
+++ vue_app/daggerheart-encounter-calculator/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Daggerheart Encounter Calculator</h1>
+    <h2>Prototype v2</h2>
+  </header>
+
+  <footer>
+      <ul class="footer-list">
+          <li>Created by&nbsp;<a class="fm-text" href="https://github.com/FedoraMark"><strong>Fedora<span>Mark</span></strong></a></li>
+          <li>Spare some&nbsp;<a class="kofi-text" href="https://ko-fi.com/fedoramark"><strong>coin?</strong></a></li>
+          <li><img src="./assets/images/compatible_with_DH_logos-07.png">&nbsp;<a href="https://www.daggerheart.com"><strong>Daggerheart</strong></a>&nbsp;compatible</li>
+      </ul>
+  </footer>
+
+</template>
+
+<style scoped>
+
+</style>
