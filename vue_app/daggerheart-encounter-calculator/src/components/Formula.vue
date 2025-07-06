1ac1375b3f52c28605fea056534e50ba52684a96
FedoraMark <fedora.mark@icloud.com>
2025-07-06 12:33:57 -0400

    [vue app] fixes and tweaks

--- vue_app/daggerheart-encounter-calculator/src/components/Formula.vue
+++ vue_app/daggerheart-encounter-calculator/src/components/Formula.vue
@@ -6,7 +6,9 @@
   <div id="Formula_Wrapper" class="formula">
     <h3>Formula</h3>
     <ul id="FormulaList" class="formula_list">
-      <li id="frm-formula" class="formula_list_item">(3 &#215; <span id="frm-pcs">{{ props.numPCs }}</span> PCs) + 2 BPs = <span id="frm-total" class="adjustment-text-static">{{ props.formulaBPs }}</span></li>
+      <li id="frm-formula" class="formula_list_item">
+        <span class="formula_list_item_wrapper">(3 &#215; <span id="frm-pcs">{{ props.numPCs }}</span> PCs) + 2 BPs = <span id="frm-total" class="adjustment-text-static">{{ props.formulaBPs }}</span></span>
+      </li>
     </ul>
   </div>
 </template>
